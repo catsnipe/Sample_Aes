e3f5cc536dd751b773819eaf65434e1b8d88843f
fujii.toybox <fujii.toybox@gmail.com>
2022-11-07 16:55:45 +0900

    first commit

---
+++ Assets/AesCbc.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AesCbc
+{
+    const int PASSWORD_LENGTH = 16;
+    const int ITERATION_COUNT = 978;
+
+    /// <summary>
+    /// 暗号化
+    /// </summary>
+    /// <param name="bytes">バイト配列</param>
+    /// <param name="password">パスワード</param>
+    /// <returns>暗号化したバイト配列</returns>
+    public static byte[] Encrypt(byte[] bytes, string password)
+    {
+        byte[] outbytes = null;
+
+        try
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.BlockSize = PASSWORD_LENGTH * 8;
+                aes.KeySize   = PASSWORD_LENGTH * 8;
+                aes.Mode      = CipherMode.CBC;
+                aes.Padding   = PaddingMode.PKCS7;
+
+                var passBytes = GetDeriveBytes(password, aes.BlockSize, aes.KeySize);
+
+                var salt      = passBytes.Salt;
+                aes.Key       = passBytes.Key;
+                aes.IV        = passBytes.IV;
+
+                byte[] outenc;
+
+                // encrypt
+                using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                {
+                    outenc = encryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+                }
+
+                // salt, IV を埋め込む
+                outbytes =
+                    new byte[
+                        outenc.Length +
+                        salt.Length +
+                        aes.IV.Length
+                    ];
+
+                int pos = 0;
+
+                // データの順番
+                byte[][] writeBytes = new byte[][]
+                {
+                    salt,
+                    outenc,
+                    aes.IV,
+                };
+
+                for (int i = 0; i < writeBytes.Length; i++)
+                {
+                    Buffer.BlockCopy(writeBytes[i], 0, outbytes, pos, writeBytes[i].Length);
+                    pos += writeBytes[i].Length;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+
+        return outbytes;
+    }
+
+    /// <summary>
+    /// 暗号化したバイト配列の複合化
+    /// </summary>
+    /// <param name="bytes">暗号化したバイト配列</param>
+    /// <param name="password">パスワード</param>
+    /// <returns>バイト配列</returns>
+    public static byte[] Decrypt(byte[] bytes, string password)
+    {
+        byte[] outbytes = null;
+
+        try
+        {
+            // decrypt
+            byte[] outenc = new byte[bytes.Length - PASSWORD_LENGTH * 2];
+            byte[] salt   = new byte[PASSWORD_LENGTH];
+            byte[] IV     = new byte[PASSWORD_LENGTH];
+
+            Buffer.BlockCopy(bytes, 0,                              salt,   0, salt.Length);
+            Buffer.BlockCopy(bytes, PASSWORD_LENGTH,                outenc, 0, outenc.Length);
+            Buffer.BlockCopy(bytes, bytes.Length - PASSWORD_LENGTH, IV,     0, IV.Length);
+
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.BlockSize = PASSWORD_LENGTH * 8;
+                aes.KeySize = PASSWORD_LENGTH * 8;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(password, salt);
+                deriveBytes.IterationCount     = ITERATION_COUNT;
+                byte[] key    = deriveBytes.GetBytes(PASSWORD_LENGTH);
+                aes.Key       = key;
+                aes.IV        = IV;
+
+                // decrypt
+                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                {
+                    outbytes = decryptor.TransformFinalBlock(outenc, 0, outenc.Length);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+
+        return outbytes;
+    }
+
+    /// <summary>
+    /// ランダムパスワードを生成
+    /// </summary>
+    /// <param name="password">パスワード</param>
+    /// <param name="keySize">キーサイズ (byte * 8)</param>
+    /// <param name="blockSize">ブロックサイズ (byte * 8)</param>
+    /// <returns></returns>
+    public static (byte[] Salt, byte[] Key, byte[] IV) GetDeriveBytes(string password, int keySize, int blockSize)
+    {
+        Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, PASSWORD_LENGTH);
+        rfc2898DeriveBytes.IterationCount     = ITERATION_COUNT;
+
+        var salt = rfc2898DeriveBytes.Salt;
+        var key  = rfc2898DeriveBytes.GetBytes(keySize / 8);
+        var IV   = rfc2898DeriveBytes.GetBytes(blockSize / 8);
+
+        return (salt, key, IV);
+    }
+}
---
+++ Assets/Sample.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Exsample
+/// </summary>
+[System.Serializable]
+public class SubEntity
+{
+    public string Name;
+    public string Data;
+}
+
+[System.Serializable]
+public class RowEntity
+{
+    public string          GUID;
+    public string          Title;
+    public string          Url;
+    public string          Memo;
+    public List<SubEntity> Subs;
+}
+
+
+public class Sample : MonoBehaviour
+{
+    [SerializeField]
+    Button      Encode;
+    [SerializeField]
+    Button      Decode;
+
+    const string APP_PASSWORD = "01234567";
+
+    void Awake()
+    {
+        Encode.onClick.AddListener(clickEncode);
+        Decode.onClick.AddListener(clickDecode);
+
+        Directory.CreateDirectory("Results");
+    }
+
+    void clickEncode()
+    {
+        RowEntity row = new RowEntity();
+
+        // exsample data
+        row.GUID = "19fecfe7-0770-41f6-9872-ab7395381979";
+        row.Title = "a8";
+        row.Url = "http://xxx.a8.net/";
+        row.Memo = "ID: 99999999\nhttp://www.a8.net/a8v2/asReminderAction.do";
+
+        row.Subs = new List<SubEntity>();
+        row.Subs.Add(new SubEntity() {Name = "ID", Data = "xxxxx"} );
+        row.Subs.Add(new SubEntity() {Name = "pass", Data = "aksesa\\\"#ed0w"} );
+
+        // 1. Class Instance -> Json
+        string json   = JsonUtility.ToJson(row, true);
+        File.WriteAllText("Results/1a_json.txt", json);
+
+        // 2. Json -> Binary
+        byte[] bin    = Encoding.ASCII.GetBytes(json);
+        File.WriteAllBytes("Results/2a_binary.bin", bin);
+
+        // 3. Encrypt
+        byte[] encbin = AesCbc.Encrypt(bin, APP_PASSWORD);
+        File.WriteAllBytes("Results/final_encrypt.bin", encbin);
+
+        Debug.Log($"encrypt data: Results/final_encrypt.bin");
+    }
+
+    void clickDecode()
+    {
+        // read encrypt file
+        byte[] encbin = File.ReadAllBytes("Results/final_encrypt.bin");
+
+        // 1. Decrypt
+        byte[] decbin = AesCbc.Decrypt(encbin, APP_PASSWORD);
+        File.WriteAllBytes("Results/2b_binary.bin", decbin);
+
+        // 2. Binary -> Json
+        string json   = Encoding.ASCII.GetString(decbin);
+        File.WriteAllText("Results/1b_json.txt", json);
+
+        // 3. Json -> Class Instance
+        var row = JsonUtility.FromJson<RowEntity>(json);
+        Debug.Log($"decrypt GUID: {row.GUID}");
+    }
+
+}
